1960b984c97d69ddc8766e0d609640b1ea5dca01
ynot24 <aaron.govinth@gmail.com>
2020-06-05 13:27:17 +0200

    Create SeanceEnseignantsDAO.java
    Ajout de la classe SeanceEnseignantsDAO

---
+++ projet/src/modele/SeanceEnseignantsDAO.java
@@ -0,0 +1,128 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modele;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+/**
+ *
+ * @author Tony
+ */
+public class SeanceEnseignantsDAO implements DAO<SeanceEnseignants>{
+    
+    @Override
+    public SeanceEnseignants find(int idConnection) {
+        
+        //Déclaration des objets
+        SeanceEnseignants ste = new SeanceEnseignants();
+        
+        //Déclaration des variables
+        String username = "root"; //Variable de type String qui stockera l'identifiant de la base de données
+        String pass = ""; //Variable de type String qui stockera son mot de passe
+        String url = "jdbc:mysql://localhost/timetable?autoReconnect=true&useSSL=false"; //Variable de type String qui stockera le lien vers la base de donnée
+        String query = "select * from seance_enseignants where id_seance=" + idConnection; //Variable de type String qui stockera la requête sql
+        
+        try {
+            //Création d'une connexion à la base de donnée
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection dbConnect = DriverManager.getConnection(url, username, pass);
+            Statement dbStatement = dbConnect.createStatement();
+            ResultSet dbResult = dbStatement.executeQuery(query);
+            if(dbResult.next()){
+                ste.id_seance = dbResult.getInt("id_seance");
+                ste.id_enseignant = dbResult.getInt("id_enseignant");
+            }
+            else{
+                System.out.println("Pas de résultat");
+            }
+            dbStatement.close();
+            dbConnect.close();
+        } catch (Exception e) {
+            System.out.println("ECHEC CONNEXION A LA BDD");
+        }
+        
+        return ste;
+    }
+
+    @Override
+    public void create(SeanceEnseignants steCreate) {
+        //Déclaration des variables
+        String username = "root"; //Variable de type String qui stockera l'identifiant de la base de données
+        String pass = ""; //Variable de type String qui stockera son mot de passe
+        String url = "jdbc:mysql://localhost/timetable?autoReconnect=true&useSSL=false"; //Variable de type String qui stockera le lien vers la base de donnée
+        String query = "insert into seance_enseignants values (?,?)"; //Variable de type String qui stockera la requête sql
+        
+        try {
+            //Création d'une connexion à la base de donnée
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection dbConnect = DriverManager.getConnection(url, username, pass);
+            PreparedStatement dbPreparedStatement = dbConnect.prepareStatement(query);
+            dbPreparedStatement.setInt(1, steCreate.id_seance);
+            dbPreparedStatement.setInt(2, steCreate.id_enseignant);
+            dbPreparedStatement.executeUpdate();
+            dbPreparedStatement.close();
+            dbConnect.close();
+        } catch (Exception e) {
+            System.out.println("ECHEC CONNEXION A LA BDD");
+        }
+    }
+
+    @Override
+    public void delete(SeanceEnseignants steDelete) {
+        
+        //Déclaration des variables
+        String username = "root"; //Variable de type String qui stockera l'identifiant de la base de données
+        String pass = ""; //Variable de type String qui stockera son mot de passe
+        String url = "jdbc:mysql://localhost/timetable?autoReconnect=true&useSSL=false"; //Variable de type String qui stockera le lien vers la base de donnée
+        String query = "delete from seance_enseignants where id_seance=" + steDelete.id_seance; //Variable de type String qui stockera la requête sql
+        
+        try {
+            //Création d'une connexion à la base de donnée
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection dbConnect = DriverManager.getConnection(url, username, pass);
+            Statement dbStatement = dbConnect.createStatement();
+            dbStatement.executeUpdate(query);
+            dbStatement.close();
+            dbConnect.close();
+        } catch (Exception e) {
+            System.out.println("ECHEC CONNEXION A LA BDD");
+        }
+    }
+
+    @Override
+    public void update(SeanceEnseignants steUpdate) {
+        
+        SeanceEnseignants steOriginal;
+        steOriginal = find(steUpdate.id_seance);
+        
+        //Déclaration des variables
+        String username = "root"; //Variable de type String qui stockera l'identifiant de la base de données
+        String pass = ""; //Variable de type String qui stockera son mot de passe
+        String url = "jdbc:mysql://localhost/timetable?autoReconnect=true&useSSL=false"; //Variable de type String qui stockera le lien vers la base de donnée
+        
+        if(steUpdate.id_enseignant != steOriginal.id_enseignant){
+            String query = "update seance_enseignants set id_enseignant=" + steUpdate.id_enseignant + "where id_seance=" + steUpdate.id_seance;
+            try {
+            //Création d'une connexion à la base de donnée
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection dbConnect = DriverManager.getConnection(url, username, pass);
+            Statement dbStatement = dbConnect.createStatement();
+            dbStatement.executeUpdate(query);
+            dbStatement.close();
+            dbConnect.close();
+            } catch (Exception e) {
+                System.out.println("ECHEC CONNEXION A LA BDD");
+            }
+        }
+        else{
+            System.out.println("Il n'y a pas de mise à jour");
+        }
+    }
+}
