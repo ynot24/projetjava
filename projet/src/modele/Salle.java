18800e39af00aae44741528a25687fc4cd3c23bc
ynot24 <aaron.govinth@gmail.com>
2020-06-05 13:26:14 +0200

    Create Salle.java
    Ajout de la classe Salle

---
+++ projet/src/modele/Salle.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modele;
+
+/**
+ *
+ * @author Tony
+ */
+public class Salle {
+    
+    //DDV
+    protected int id;
+    protected String nom;
+    protected int capacite;
+    protected int id_site;
+    
+    //Constructeur par défaut
+    public Salle (){};
+    
+    //Constructeur à paramètres
+    public Salle (int id_c, String nom_c, int capacite_c, int id_site_c){
+        this.id = id_c;
+        this.nom = nom_c;
+        this.capacite = capacite_c;
+        this.id_site = id_site_c;
+    }
+    
+    //Accesseurs et mutateurs
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public int getCapacite() {
+        return capacite;
+    }
+
+    public void setCapacite(int capacite) {
+        this.capacite = capacite;
+    }
+
+    public int getId_site() {
+        return id_site;
+    }
+
+    public void setId_site(int id_site) {
+        this.id_site = id_site;
+    }
+    
+}
