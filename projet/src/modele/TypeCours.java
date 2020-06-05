791bf2aee8153a1e13a43fdac8a86736751bbd2c
ynot24 <aaron.govinth@gmail.com>
2020-06-05 13:28:32 +0200

    Create TypeCours.java
    Ajout de la classe TypeCours

---
+++ projet/src/modele/TypeCours.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modele;
+
+/**
+ *
+ * @author Tony
+ */
+public class TypeCours {
+    
+    //DDV
+    protected int id;
+    protected String nom;
+    
+    //Constructeur par défaut
+    public TypeCours (){};
+    
+    //Constructeur à paramètres
+    public TypeCours (int id_type, String nom_type){
+        this.id = id_type;
+        this.nom = nom_type;
+    }
+    
+    //Accesseurs et mutateurs
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+}
