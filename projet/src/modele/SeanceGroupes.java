e0a73adc1a4c7293cc61e982600e4375dbac8350
ynot24 <aaron.govinth@gmail.com>
2020-06-05 13:27:27 +0200

    Create SeanceGroupes.java
    Ajout de la classe SeanceGroupes

---
+++ projet/src/modele/SeanceGroupes.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modele;
+
+/**
+ *
+ * @author Tony
+ */
+public class SeanceGroupes {
+    
+    //DDV
+    protected int id_seance;
+    protected int id_groupe;
+    
+    //Constructeur par défaut
+    public SeanceGroupes (){};
+    
+    //Constructeur à paramètres
+    public SeanceGroupes (int id_seance_c, int id_groupe_c){
+        this.id_seance = id_seance_c;
+        this.id_groupe = id_groupe_c;
+    }
+    
+    //Accesseurs et mutateurs
+    
+    public int getId_seance() {
+        return id_seance;
+    }
+
+    public void setId_seance(int id_seance) {
+        this.id_seance = id_seance;
+    }
+
+    public int getId_groupe() {
+        return id_groupe;
+    }
+
+    public void setId_groupe(int id_groupe) {
+        this.id_groupe = id_groupe;
+    }
+    
+}
