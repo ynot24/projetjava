df932b4f963ed7a1e67e98cd8f0ab19a753dddd3
ynot24 <aaron.govinth@gmail.com>
2020-06-05 13:24:06 +0200

    Create DAO.java
    Ajout de l'interface DAO

---
+++ projet/src/modele/DAO.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modele;
+
+/**
+ *
+ * @author Tony
+ * @param <T>
+ */
+public interface DAO<T> {
+    
+    /**
+    * Méthode de recherche des informations
+    * @param id
+    * @return T
+    */
+    public T find(int id);
+    
+    /**
+    * Méthode de création
+    * @param t
+    */
+    public void create(T t);
+
+    /**
+    * Méthode pour effacer
+    * @param t
+    */
+    public void delete(T t);
+
+    /**
+    * Méthode de mise à jour
+    * @param t
+    */
+    public void update(T t);
+    
+}
