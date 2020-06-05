e861928126b728509bc2b218569c4792d4414515
ynot24 <aaron.govinth@gmail.com>
2020-06-05 13:28:43 +0200

    Create TypeCoursDAO.java
    Ajout de la classe TypeCoursDAO

---
+++ projet/src/modele/TypeCoursDAO.java
@@ -0,0 +1,125 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modele;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+/**
+ *
+ * @author Tony
+ */
+public class TypeCoursDAO implements DAO<TypeCours>{
+    
+    @Override
+    public TypeCours find(int idConnection) {
+        
+        //Déclaration des objets
+        TypeCours t = new TypeCours();
+        
+        //Déclaration des variables
+        String username = "root"; //Variable de type String qui stockera l'identifiant de la base de données
+        String pass = ""; //Variable de type String qui stockera son mot de passe
+        String url = "jdbc:mysql://localhost/timetable?autoReconnect=true&useSSL=false"; //Variable de type String qui stockera le lien vers la base de donnée
+        String query = "select * from type_cours where id=" + idConnection; //Variable de type String qui stockera la requête sql
+        
+        try {
+            //Création d'une connexion à la base de donnée
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection dbConnect = DriverManager.getConnection(url, username, pass);
+            Statement dbStatement = dbConnect.createStatement();
+            ResultSet dbResult = dbStatement.executeQuery(query);
+            if(dbResult.next()){
+                t.id = dbResult.getInt("id");
+                t.nom = dbResult.getString("nom");
+            }
+            else{
+                System.out.println("Pas de résultat");
+            }
+            dbStatement.close();
+            dbConnect.close();
+        } catch (Exception e) {
+            System.out.println("ECHEC CONNEXION A LA BDD");
+        }
+        
+        return t;
+    }
+
+    @Override
+    public void create(TypeCours typeCreate) {
+        //Déclaration des variables
+        String username = "root"; //Variable de type String qui stockera l'identifiant de la base de données
+        String pass = ""; //Variable de type String qui stockera son mot de passe
+        String url = "jdbc:mysql://localhost/timetable?autoReconnect=true&useSSL=false"; //Variable de type String qui stockera le lien vers la base de donnée
+        String query = "insert into type_cours values (?," + typeCreate.nom + ")"; //Variable de type String qui stockera la requête sql
+        
+        try {
+            //Création d'une connexion à la base de donnée
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection dbConnect = DriverManager.getConnection(url, username, pass);
+            Statement dbStatement = dbConnect.createStatement();
+            dbStatement.executeUpdate(query);
+            dbStatement.close();
+            dbConnect.close();
+        } catch (Exception e) {
+            System.out.println("ECHEC CONNEXION A LA BDD");
+        }
+    }
+
+    @Override
+    public void delete(TypeCours typeDelete) {
+        
+        //Déclaration des variables
+        String username = "root"; //Variable de type String qui stockera l'identifiant de la base de données
+        String pass = ""; //Variable de type String qui stockera son mot de passe
+        String url = "jdbc:mysql://localhost/timetable?autoReconnect=true&useSSL=false"; //Variable de type String qui stockera le lien vers la base de donnée
+        String query = "delete from type_cours where id=" + typeDelete.id; //Variable de type String qui stockera la requête sql
+        
+        try {
+            //Création d'une connexion à la base de donnée
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection dbConnect = DriverManager.getConnection(url, username, pass);
+            Statement dbStatement = dbConnect.createStatement();
+            dbStatement.executeUpdate(query);
+            dbStatement.close();
+            dbConnect.close();
+        } catch (Exception e) {
+            System.out.println("ECHEC CONNEXION A LA BDD");
+        }
+    }
+
+    @Override
+    public void update(TypeCours typeUpdate) {
+        
+        TypeCours typeOriginal;
+        typeOriginal = find(typeUpdate.id);
+        
+        //Déclaration des variables
+        String username = "root"; //Variable de type String qui stockera l'identifiant de la base de données
+        String pass = ""; //Variable de type String qui stockera son mot de passe
+        String url = "jdbc:mysql://localhost/timetable?autoReconnect=true&useSSL=false"; //Variable de type String qui stockera le lien vers la base de donnée
+        
+        if(typeUpdate.nom != typeOriginal.nom){
+            String query = "update type_update set email=" + typeUpdate.nom + "where id=" + typeUpdate.id;
+            try {
+            //Création d'une connexion à la base de donnée
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection dbConnect = DriverManager.getConnection(url, username, pass);
+            Statement dbStatement = dbConnect.createStatement();
+            dbStatement.executeUpdate(query);
+            dbStatement.close();
+            dbConnect.close();
+            } catch (Exception e) {
+                System.out.println("ECHEC CONNEXION A LA BDD");
+            }
+        }
+        else{
+            System.out.println("Il n'y a pas de mise à jour");
+        }
+    }
+}
