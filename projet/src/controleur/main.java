b5b6c69c84b6c7f677661cfd4e3c5462b58cb4e0
ynot24 <aaron.govinth@gmail.com>
2020-06-07 20:41:34 +0200

    MAJ

---
+++ projet/src/controleur/main.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controleur;
+
+import vue.*;
+
+
+/**
+ * Classe Main qui interagit avec les deux packages
+ *
+ * @author Tony
+ */
+
+public class main {
+    
+    public static void main(String[] args) {
+        Login login = new Login();
+    }
+}
