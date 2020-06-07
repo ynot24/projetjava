f1f7e6013f1e5748e85279c2998c9c3922e7e12b
ynot24 <aaron.govinth@gmail.com>
2020-06-07 03:47:16 +0200

    Update SeanceSallesDAO.java
    MAJ

--- projet/src/modele/SeanceSallesDAO.java
+++ projet/src/modele/SeanceSallesDAO.java
@@ -36,8 +36,8 @@ public class SeanceSallesDAO implements DAO<SeanceSalles>{
             Statement dbStatement = dbConnect.createStatement();
             ResultSet dbResult = dbStatement.executeQuery(query);
             if(dbResult.next()){
-                ss.id_seance = dbResult.getInt("id_seance");
-                ss.id_salle = dbResult.getInt("id_salle");
+                ss.setId_seance(dbResult.getInt("id_seance"));
+                ss.setId_salle(dbResult.getInt("id_salle"));
             }
             else{
                 System.out.println("Pas de résultat");
@@ -64,8 +64,8 @@ public class SeanceSallesDAO implements DAO<SeanceSalles>{
             Class.forName("com.mysql.jdbc.Driver");
             Connection dbConnect = DriverManager.getConnection(url, username, pass);
             PreparedStatement dbPreparedStatement = dbConnect.prepareStatement(query);
-            dbPreparedStatement.setInt(1, ssCreate.id_seance);
-            dbPreparedStatement.setInt(2, ssCreate.id_salle);
+            dbPreparedStatement.setInt(1, ssCreate.getId_seance());
+            dbPreparedStatement.setInt(2, ssCreate.getId_salle());
             dbPreparedStatement.executeUpdate();
             dbPreparedStatement.close();
             dbConnect.close();
@@ -81,7 +81,7 @@ public class SeanceSallesDAO implements DAO<SeanceSalles>{
         String username = "root"; //Variable de type String qui stockera l'identifiant de la base de données
         String pass = ""; //Variable de type String qui stockera son mot de passe
         String url = "jdbc:mysql://localhost/timetable?autoReconnect=true&useSSL=false"; //Variable de type String qui stockera le lien vers la base de donnée
-        String query = "delete from seance_salles where id_seance=" + ssDelete.id_seance; //Variable de type String qui stockera la requête sql
+        String query = "delete from seance_salles where id_seance=" + ssDelete.getId_seance(); //Variable de type String qui stockera la requête sql
         
         try {
             //Création d'une connexion à la base de donnée
@@ -100,15 +100,15 @@ public class SeanceSallesDAO implements DAO<SeanceSalles>{
     public void update(SeanceSalles ssUpdate) {
         
         SeanceSalles ssOriginal;
-        ssOriginal = find(ssUpdate.id_seance);
+        ssOriginal = find(ssUpdate.getId_seance());
         
         //Déclaration des variables
         String username = "root"; //Variable de type String qui stockera l'identifiant de la base de données
         String pass = ""; //Variable de type String qui stockera son mot de passe
         String url = "jdbc:mysql://localhost/timetable?autoReconnect=true&useSSL=false"; //Variable de type String qui stockera le lien vers la base de donnée
         
-        if(ssUpdate.id_salle != ssOriginal.id_salle){
-            String query = "update seance_salles set id_salle=" + ssUpdate.id_salle + "where id_seance=" + ssUpdate.id_seance;
+        if(ssUpdate.getId_salle() != ssOriginal.getId_salle()){
+            String query = "update seance_salles set id_salle=" + ssUpdate.getId_salle() + " where id_seance=" + ssUpdate.getId_seance();
             try {
             //Création d'une connexion à la base de donnée
             Class.forName("com.mysql.jdbc.Driver");
